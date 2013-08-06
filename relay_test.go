7a4977e7810ed4baff7465d0669d49cb82e1274d
Armon Dadgar <armon.dadgar@gmail.com>
2013-08-06 15:34:09 -0700

    More integ tests

--- relay_test.go
+++ relay_test.go
@@ -9,25 +9,14 @@ import (
 )
 
 func IsInteg() bool {
-	return os.Getenv("INTEG_TESTS") == "true"
+	return os.Getenv("INTEG_TESTS") == "true" && AMQPHost() != ""
 }
 
 func AMQPHost() string {
 	return os.Getenv("AMQP_HOST")
 }
 
-func TestSimplePublishConsume(t *testing.T) {
-	if !IsInteg() {
-		t.SkipNow()
-	}
-
-	conf := Config{Addr: AMQPHost()}
-	r, err := New(&conf)
-	if err != nil {
-		t.Fatalf("unexpected err %s", err)
-	}
-	defer r.Close()
-
+func testSendRecv(t *testing.T, r *Relay) {
 	// Get a publisher
 	pub, err := r.Publisher("test")
 	if err != nil {
@@ -56,9 +45,205 @@ func TestSimplePublishConsume(t *testing.T) {
 		t.Fatalf("unexpected err %s", err)
 	}
 
+	// Ack the message
+	err = cons.Ack()
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+
 	// Check message
 	if in != msg {
 		t.Fatalf("unexpected msg! %v %v", in, msg)
+	}
+}
+
+func TestSimplePublishConsume(t *testing.T) {
+	if !IsInteg() {
+		t.SkipNow()
+	}
+
+	conf := Config{Addr: AMQPHost()}
+	r, err := New(&conf)
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+	defer r.Close()
+
+	testSendRecv(t, r)
+}
+
+func TestPublishNoConfirm(t *testing.T) {
+	if !IsInteg() {
+		t.SkipNow()
+	}
+
+	conf := Config{Addr: AMQPHost(), DisablePublishConfirm: true}
+	r, err := New(&conf)
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+	defer r.Close()
+
+	testSendRecv(t, r)
+}
+
+func TestPublishNoPersist(t *testing.T) {
+	if !IsInteg() {
+		t.SkipNow()
+	}
+
+	conf := Config{Addr: AMQPHost(), DisablePersistence: true}
+	r, err := New(&conf)
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+	defer r.Close()
+
+	testSendRecv(t, r)
+}
+
+func TestCustomExchange(t *testing.T) {
+	if !IsInteg() {
+		t.SkipNow()
+	}
+
+	conf := Config{Addr: AMQPHost(), Exchange: "my-exchange"}
+	r, err := New(&conf)
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+	defer r.Close()
+
+	testSendRecv(t, r)
+}
+
+func TestRelayMultiClose(t *testing.T) {
+	if !IsInteg() {
+		t.SkipNow()
+	}
+
+	conf := Config{Addr: AMQPHost()}
+	r, err := New(&conf)
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+	if err := r.Close(); err != nil {
+		t.Fatalf("unexpected err")
+	}
+	if err := r.Close(); err != nil {
+		t.Fatalf("unexpected err")
+	}
+}
+
+func TestConsumerMultiClose(t *testing.T) {
+	if !IsInteg() {
+		t.SkipNow()
+	}
+
+	conf := Config{Addr: AMQPHost()}
+	r, err := New(&conf)
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+
+	cons, err := r.Consumer("test")
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+
+	if err := cons.Close(); err != nil {
+		t.Fatalf("unexpected err")
+	}
+	if err := cons.Close(); err != nil {
+		t.Fatalf("unexpected err")
+	}
+}
+
+func TestPublisherMultiClose(t *testing.T) {
+	if !IsInteg() {
+		t.SkipNow()
+	}
+
+	conf := Config{Addr: AMQPHost()}
+	r, err := New(&conf)
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+
+	pub, err := r.Publisher("test")
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+
+	if err := pub.Close(); err != nil {
+		t.Fatalf("unexpected err")
+	}
+	if err := pub.Close(); err != nil {
+		t.Fatalf("unexpected err")
+	}
+}
+
+func TestMultiConsume(t *testing.T) {
+	if !IsInteg() {
+		t.SkipNow()
+	}
+
+	conf := Config{Addr: AMQPHost(), PrefetchCount: 5, EnableMultiAck: true}
+	r, err := New(&conf)
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+	defer r.Close()
+
+	// Get a publisher
+	pub, err := r.Publisher("test")
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+	defer pub.Close()
+
+	// Get a consumer
+	cons, err := r.Consumer("test")
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+	defer cons.Close()
+
+	// Send a message
+	for i := 0; i < 5; i++ {
+		err = pub.Publish(string(i))
+		if err != nil {
+			t.Fatalf("unexpected err %s", err)
+		}
+	}
+
+	// Try to get the message
+	var in string
+	for i := 0; i < 5; i++ {
+		err = cons.Consume(&in)
+		if err != nil {
+			t.Fatalf("unexpected err %s", err)
+		}
+		if in != string(i) {
+			t.Fatalf("unexpected msg! %v %v", in, i)
+		}
+	}
+
+	// Nack all the messages
+	err = cons.Nack()
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+
+	// Should redeliver
+	for i := 0; i < 5; i++ {
+		err = cons.ConsumeAck(&in)
+		if err != nil {
+			t.Fatalf("unexpected err %s", err)
+		}
+		if in != string(i) {
+			t.Fatalf("unexpected msg! %v %v", in, i)
+		}
 	}
 }
 
