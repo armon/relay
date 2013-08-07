a95e9b523f452bd68dcead0f88d82d01a83c3683
Armon Dadgar <armon.dadgar@gmail.com>
2013-08-07 10:19:20 -0700

    Move the util tests

--- relay_test.go
+++ relay_test.go
@@ -491,26 +491,3 @@ func TestNoHost(t *testing.T) {
 		t.Fatalf("expected err!")
 	}
 }
-
-func TestQueueName(t *testing.T) {
-	if queueName("test") != "relay.test" {
-		t.Fatalf("bad queue name")
-	}
-}
-
-func TestChannelName(t *testing.T) {
-	names := make(map[string]struct{})
-	for i := 0; i < 100; i++ {
-		name, err := channelName()
-		if err != nil {
-			t.Fatalf("unexpected err %s", err)
-		}
-		if name == "" {
-			t.Fatalf("expected name")
-		}
-		if _, ok := names[name]; ok {
-			t.Fatalf("expected unique name!")
-		}
-		names[name] = struct{}{}
-	}
-}
